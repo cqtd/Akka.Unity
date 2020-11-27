e74cd16182a9e9d63fa47d8b10842a36541e0398
cqtd <cqtd@protonmail.ch>
2020-11-27 20:43:33 +0900

    Akka for Unity Sample 19.1.9f1

---
+++ AkkaServer/AkkaServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AkkaServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
